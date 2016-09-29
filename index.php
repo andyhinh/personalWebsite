70ad82eed32f103db96f53040c2a9fd276839a81
Andy <andywhinh@gmail.com>
2016-09-28 21:46:33 -0500

    Fixing

--- index.php
+++ index.php
@@ -268,7 +268,7 @@
 						</div>
 
             <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
               $name=$_POST['name'];
               $email=$_POST['email'];
               $message=$_POST['message'];
