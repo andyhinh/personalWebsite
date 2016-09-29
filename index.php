bf497a709a5b15a050d350b5dd93081429b8d21c
Andy <andywhinh@gmail.com>
2016-09-29 02:03:29 -0500

    bug;

--- index.php
+++ index.php
@@ -268,7 +268,6 @@
 						</div>
 
             <?php
-            require_once "Mail.php";
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
               $name=$_POST['name'];
               $email=$_POST['email'];
